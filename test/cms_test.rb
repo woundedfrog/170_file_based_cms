d2c7a1c4c0741c2a54992c025131b29dd0d71bc5
Bernard Maisel <ntjust26@gmail.com>
2018-08-24 19:18:54 +0900

    Test files
    Added test files and basic tests.

---
+++ test/cms_test.rb
@@ -0,0 +1,21 @@
+ENV["RACK_ENV"] = "test"
+
+require "minitest/autorun"
+require "rack/test"
+
+require_relative "../cms"
+
+class CMSTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_index
+    get "/"
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "about.txt"
+  end
+end
